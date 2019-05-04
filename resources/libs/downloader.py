9a2c9f9de76c1a256f41140b0124ed443c74fd75
drinfernoo <2319508+drinfernoo@users.noreply.github.com>
2019-05-04 10:10:44 -0700

    :recycle::lipstick: - Rewrite downloader

--- resources/libs/downloader.py
+++ resources/libs/downloader.py
@@ -1,5 +1,5 @@
 ################################################################################
-#      Copyright (C) 2015 Surfacingx                                           #
+#      Copyright (C) 2019 drinfernoo                                           #
 #                                                                              #
 #  This Program is free software; you can redistribute it and/or modify        #
 #  it under the terms of the GNU General Public License as published by        #
@@ -18,52 +18,53 @@
 ################################################################################
 
 import xbmc
-import xbmcgui
 
-import urllib
 import sys
 import time
 
+try:  # Python 3
+    from urllib.request import urlretrieve
+except ImportError:  # Python 2
+    from urllib import urlretrieve
+
 import uservar
-from resources.libs import wizard as wiz
+from resources.libs import logging
+from resources.libs import tools
 
-ADDONTITLE     = uservar.ADDONTITLE
-COLOR1         = uservar.COLOR1
-COLOR2         = uservar.COLOR2
 
-urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
+def download(url, dest, dp=None):
+    if not dp:
+        dp = tools.DP
+        dp.create(uservar.ADDONTITLE, "Downloading Content", ' ', ' ')
+    dp.update(0)
+    start_time = time.time()
+    urlretrieve(url, dest, lambda nb, bs, fs: _pbhook(nb, bs, fs, dp, start_time))
 
-def download(url, dest, dp = None):
-	if not dp:
-		dp = xbmcgui.DialogProgress()
-		dp.create(ADDONTITLE ,"Downloading Content",' ', ' ')
-	dp.update(0)
-	start_time=time.time()
-	urllib.urlretrieve(url, dest, lambda nb, bs, fs: _pbhook(nb, bs, fs, dp, start_time))
 
 def _pbhook(numblocks, blocksize, filesize, dp, start_time):
-	try: 
-		percent = min(numblocks * blocksize * 100 / filesize, 100) 
-		currently_downloaded = float(numblocks) * blocksize / (1024 * 1024) 
-		kbps_speed = numblocks * blocksize / (time.time() - start_time) 
-		if kbps_speed > 0 and not percent == 100: 
-			eta = (filesize - numblocks * blocksize) / kbps_speed 
-		else: 
-			eta = 0
-		kbps_speed = kbps_speed / 1024 
-		type_speed = 'KB'
-		if kbps_speed >= 1024:
-			kbps_speed = kbps_speed / 1024 
-			type_speed = 'MB'
-		total = float(filesize) / (1024 * 1024) 
-		mbs = '[COLOR %s][B]Size:[/B] [COLOR %s]%.02f[/COLOR] MB of [COLOR %s]%.02f[/COLOR] MB[/COLOR]' % (COLOR2, COLOR1, currently_downloaded, COLOR1, total) 
-		e   = '[COLOR %s][B]Speed:[/B] [COLOR %s]%.02f [/COLOR]%s/s ' % (COLOR2, COLOR1, kbps_speed, type_speed)
-		e  += '[B]ETA:[/B] [COLOR '+COLOR1+']%02d:%02d[/COLOR][/COLOR]' % divmod(eta, 60)
-		dp.update(percent, '', mbs, e)
-	except Exception, e:
-		wiz.log("ERROR Downloading: %s" % str(e), xbmc.LOGERROR)
-		return str(e)
-	if dp.iscanceled(): 
-		dp.close()
-		wiz.LogNotify("[COLOR %s]%s[/COLOR]" % (COLOR1, ADDONTITLE), "[COLOR %s]Download Cancelled[/COLOR]" % COLOR2)
-		sys.exit()
+    try:
+        percent = min(numblocks * blocksize * 100 / filesize, 100)
+        currently_downloaded = float(numblocks) * blocksize / (1024 * 1024)
+        kbps_speed = numblocks * blocksize / (time.time() - start_time)
+        if kbps_speed > 0 and not percent == 100:
+            eta = (filesize - numblocks * blocksize) / kbps_speed
+        else:
+            eta = 0
+        kbps_speed = kbps_speed / 1024
+        type_speed = 'KB'
+        if kbps_speed >= 1024:
+            kbps_speed = kbps_speed / 1024
+            type_speed = 'MB'
+        total = float(filesize) / (1024 * 1024)
+        mbs = '[COLOR %s][B]Size:[/B] [COLOR %s]%.02f[/COLOR] MB of [COLOR %s]%.02f[/COLOR] MB[/COLOR]' % (uservar.COLOR2, uservar.COLOR1, currently_downloaded, uservar.COLOR1, total)
+        e = '[COLOR %s][B]Speed:[/B] [COLOR %s]%.02f [/COLOR]%s/s ' % (uservar.COLOR2, uservar.COLOR1, kbps_speed, type_speed)
+        e += '[B]ETA:[/B] [COLOR ' + uservar.COLOR1 + ']%02d:%02d[/COLOR][/COLOR]' % divmod(eta, 60)
+        dp.update(percent, '', mbs, e)
+    except Exception as e:
+        logging.log("ERROR Downloading: %s".format(str(e)), level=xbmc.LOGERROR)
+        return str(e)
+    if dp.iscanceled():
+        dp.close()
+        logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(uservar.COLOR1, uservar.ADDONTITLE),
+                           "[COLOR {0}]Download Cancelled[/COLOR]".format(uservar.COLOR2))
+        sys.exit()
