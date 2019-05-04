cc40fc54cbb331a1ef5a2ffd0f2dfc3e5abc1d26
drinfernoo <2319508+drinfernoo@users.noreply.github.com>
2019-05-04 10:10:29 -0700

    :recycle::heavy_plus_sign: - Create logging, tools, and vars

---
+++ resources/libs/tools.py
@@ -0,0 +1,48 @@
+import xbmcgui
+
+
+DIALOG = xbmcgui.Dialog()
+DP = xbmcgui.DialogProgress()
+
+
+def get_setting(name):
+    try:
+        return vars.ADDON.getSetting(name)
+    except:
+        return False
+
+
+def set_setting(name, value):
+    try:
+        vars.ADDON.setSetting(name, value)
+    except:
+        return False
+
+
+def read_from_file(file):
+    f = open(file)
+    a = f.read()
+    f.close()
+    lines = a.split('\n')
+    return lines
+
+
+def write_to_file(file, content):
+    f = open(file, 'w')
+    f.write(content)
+    f.close()
+
+
+def get_date(days=0, now=False):
+    from datetime import date
+    from datetime import datetime
+    from datetime import timedelta
+
+    if not now:
+        if days == 0:
+            return date.today()
+        else:
+            return date.today() + timedelta(days)
+    else:
+        return datetime.now()
+
---
+++ resources/libs/vars.py
@@ -0,0 +1,56 @@
+import xbmc
+import xbmcaddon
+
+import os
+
+import uservar
+from resources.libs import tools
+
+# Add-on metadata-related variables
+ADDON = xbmcaddon.Addon(uservar.ADDON_ID)
+VERSION = ADDON.getAddonInfo('version')
+KODIV = float(xbmc.getInfoLabel("System.BuildVersion")[:4])
+
+
+# Path-related variables
+HOME = xbmc.translatePath('special://home/')
+XBMC = xbmc.translatePath('special://xbmc/')
+LOG = xbmc.translatePath('special://logpath/')
+PROFILE = xbmc.translatePath('special://profile/')
+TEMPDIR = xbmc.translatePath('special://temp')
+
+ADDONS = os.path.join(HOME, 'addons')
+USERDATA = os.path.join(HOME, 'userdata')
+PLUGIN = os.path.join(ADDONS, uservar.ADDON_ID)
+PACKAGES = os.path.join(ADDONS, 'packages')
+ADDOND = os.path.join(USERDATA, 'addon_data')
+ADDONDATA = os.path.join(USERDATA, 'addon_data', uservar.ADDON_ID)
+THUMBS = os.path.join(USERDATA, 'Thumbnails')
+DATABASE = os.path.join(USERDATA, 'Database')
+QRCODES = os.path.join(ADDONDATA, 'QRCodes')
+TEXTCACHE = os.path.join(ADDONDATA, 'Cache')
+ARCHIVE_CACHE = os.path.join(TEMPDIR, 'archive_cache')
+ART = os.path.join(PLUGIN, 'resources', 'art')
+SKIN = xbmc.getSkinDir()
+BACKUPLOCATION = tools.get_setting('path') if not tools.get_setting('path') == '' else HOME
+MYBUILDS = os.path.join(BACKUPLOCATION, 'My_Builds')
+
+
+# File-related variables
+ADVANCED = os.path.join(USERDATA, 'advancedsettings.xml')
+SOURCES = os.path.join(USERDATA, 'sources.xml')
+GUISETTINGS = os.path.join(USERDATA, 'guisettings.xml')
+FAVOURITES = os.path.join(USERDATA, 'favourites.xml')
+PROFILES = os.path.join(USERDATA, 'profiles.xml')
+FANART = os.path.join(PLUGIN, 'fanart.jpg')
+ICON = os.path.join(PLUGIN, 'icon.png')
+WIZLOG = os.path.join(ADDONDATA, 'wizard.log')
+WHITELIST = os.path.join(ADDONDATA, 'whitelist.txt')
+
+
+# Misc variables
+DEFAULTPLUGINS = ['metadata.album.universal', 'metadata.artists.universal', 'metadata.common.fanart.tv',
+                  'metadata.common.imdb.com', 'metadata.common.musicbrainz.org', 'metadata.themoviedb.org',
+                  'metadata.tvdb.com', 'service.xbmc.versioncheck']
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
