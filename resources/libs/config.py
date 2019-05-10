3a246f08ec986a14e0a679567d4aa374cfa28868
drinfernoo <2319508+drinfernoo@users.noreply.github.com>
2019-05-10 10:51:55 -0700

    :heavy_plus_sign: - More variables into config

--- resources/libs/config.py
+++ resources/libs/config.py
@@ -94,6 +94,9 @@ class Config:
                                'metadata.tvdb.com', 'service.xbmc.versioncheck']
         self.USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)' \
                           'Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
+        self.BAD_FILES = ['onechannelcache.db', 'saltscache.db', 'saltscache.db-shm', 'saltscache.db-wal',
+                          'saltshd.lite.db', 'saltshd.lite.db-shm', 'saltshd.lite.db-wal', 'queue.db', 'commoncache.db',
+                          'access.log', 'trakt.db', 'video_cache.db', '.gitignore', '.DS_Store']
 
         # Default special paths
         self.XBMC = xbmc.translatePath('special://xbmc/')
