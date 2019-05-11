e9e3bbbcb04cef8982e398483cdce6d86fa7a1a1
drinfernoo <2319508+drinfernoo@users.noreply.github.com>
2019-05-11 10:52:08 -0700

    :building_construction: - Migrate whitelist to config

--- resources/libs/whitelist.py
+++ resources/libs/whitelist.py
@@ -5,11 +5,10 @@ import glob
 import os
 import re
 
-import uservar
+from resources.libs.config import CONFIG
 from resources.libs import gui
 from resources.libs import logging
 from resources.libs import tools
-from resources.libs import vars
 
 
 def parse(file, foldername):
@@ -29,12 +28,12 @@ def whitelist(do):
     addonfolds = []
 
     if do == 'edit':
-        fold = glob.glob(os.path.join(vars.ADDONS, '*/'))
+        fold = glob.glob(os.path.join(CONFIG.ADDONS, '*/'))
         for folder in sorted(fold, key=lambda x: x):
             foldername = os.path.split(folder[:-1])[1]
-            if foldername in uservar.EXCLUDES:
+            if foldername in CONFIG.EXCLUDES:
                 continue
-            elif foldername in vars.DEFAULTPLUGINS:
+            elif foldername in CONFIG.DEFAULTPLUGINS:
                 continue
             elif foldername == 'packages':
                 continue
@@ -45,15 +44,15 @@ def whitelist(do):
                 addonnames.append(temp)
                 addonids.append(addid)
                 addonfolds.append(foldername)
-        fold2 = glob.glob(os.path.join(vars.ADDOND, '*/'))
+        fold2 = glob.glob(os.path.join(CONFIG.ADDON_DATA, '*/'))
         for folder in sorted(fold2, key=lambda x: x):
             foldername = os.path.split(folder[:-1])[1]
             if foldername in addonfolds:
                 continue
-            if foldername in uservar.EXCLUDES:
+            if foldername in CONFIG.EXCLUDES:
                 continue
-            xml = os.path.join(vars.ADDONS, foldername, 'addon.xml')
-            xml2 = os.path.join(vars.XBMC, 'addons', foldername, 'addon.xml')
+            xml = os.path.join(CONFIG.ADDONS, foldername, 'addon.xml')
+            xml2 = os.path.join(CONFIG.XBMC, 'addons', foldername, 'addon.xml')
             if os.path.exists(xml):
                 a = tools.read_from_file(xml)
             elif os.path.exists(xml2):
@@ -68,7 +67,7 @@ def whitelist(do):
         tempaddonnames = ["-- Click here to Continue --"] + addonnames
         currentWhite = whitelist(do='read')
         for item in currentWhite:
-            logging.log(str(item), xbmc.LOGDEBUG)
+            logging.log(str(item))
             try:
                 name, id, fold = item
             except Exception as e:
@@ -76,14 +75,14 @@ def whitelist(do):
             if id in addonids:
                 pos = addonids.index(id)+1
                 selected.append(pos-1)
-                tempaddonnames[pos] = "[B][COLOR %s]%s[/COLOR][/B]" % (uservar.COLOR1, name)
+                tempaddonnames[pos] = "[B][COLOR {0}]{1}[/COLOR][/B]".format(CONFIG.COLOR1, name)
             else:
                 addonids.append(id)
                 addonnames.append(name)
-                tempaddonnames.append("[B][COLOR %s]%s[/COLOR][/B]" % (uservar.COLOR1, name))
+                tempaddonnames.append("[B][COLOR {0}]{1}[/COLOR][/B]".format(CONFIG.COLOR1, name))
         choice = 1
         while choice not in [-1, 0]:
-            choice = gui.DIALOG.select("%s: Select the addons you wish to White List." % uservar.ADDONTITLE, tempaddonnames)
+            choice = gui.DIALOG.select("{0}: Select the add-ons you wish to the whitelist.".format(CONFIG.ADDONTITLE), tempaddonnames)
             if choice == -1:
                 break
             elif choice == 0:
@@ -95,23 +94,24 @@ def whitelist(do):
                     tempaddonnames[choice] = addonnames[choice2]
                 else:
                     selected.append(choice2)
-                    tempaddonnames[choice] = "[B][COLOR %s]%s[/COLOR][/B]" % (uservar.COLOR1, addonnames[choice2])
+                    tempaddonnames[choice] = "[B][COLOR {0}]{1}[/COLOR][/B]".format(CONFIG.COLOR1, addonnames[choice2])
         white_list = []
         if len(selected) > 0:
             for addon in selected:
                 white_list.append("['%s', '%s', '%s']" % (addonnames[addon], addonids[addon], addonfolds[addon]))
             writing = '\n'.join(white_list)
-            tools.write_to_file(vars.WHITELIST, writing)
+            tools.write_to_file(CONFIG.WHITELIST, writing)
         else:
             try:
-                os.remove(vars.WHITELIST)
+                os.remove(CONFIG.WHITELIST)
             except:
                 pass
-        logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]%s Addons in White List[/COLOR]" % (uservar.COLOR2, len(selected)))
+        logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                           "[COLOR {0}]{1} Add-ons in whitelist[/COLOR]".format(CONFIG.COLOR2, len(selected)))
     elif do == 'read':
         white_list = []
-        if os.path.exists(vars.WHITELIST):
-            lines = tools.read_from_file(vars.WHITELIST).split('\n')
+        if os.path.exists(CONFIG.WHITELIST):
+            lines = tools.read_from_file(CONFIG.WHITELIST).split('\n')
             for item in lines:
                 try:
                     name, id, fold = eval(item)
@@ -128,15 +128,18 @@ def whitelist(do):
                     name, id, fold = item
                 except Exception as e:
                     logging.log(str(e))
-                msg += "[COLOR %s]%s[/COLOR] [COLOR %s]\"%s\"[/COLOR][CR]" % (uservar.COLOR1, name, uservar.COLOR2, id)
-            gui.TextBox("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), msg)
+                msg += "[COLOR {0}]{1][/COLOR] [COLOR {2}]\"{3}\"[/COLOR][CR]".format(CONFIG.COLOR1, name, CONFIG.COLOR2, id)
+            gui.TextBox("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE), msg)
         else:
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]No items in White List[/COLOR]" % uservar.COLOR2)
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]No items in whitelist[/COLOR]".format(CONFIG.COLOR2))
     elif do == 'import':
-        source = gui.DIALOG.browse(1, '[COLOR %s]Select the whitelist file to import[/COLOR]' % uservar.COLOR2, 'files', '.txt', False, False, vars.HOME)
+        source = gui.DIALOG.browse(1, '[COLOR {0}]Select the whitelist file to import[/COLOR]'.format(CONFIG.COLOR2),
+                                   'files', '.txt', False, False, CONFIG.HOME)
         logging.log(str(source))
         if not source.endswith('.txt'):
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]Import Cancelled![/COLOR]" % uservar.COLOR2)
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]Import Cancelled![/COLOR]".format(CONFIG.COLOR2))
             return
         current = whitelist(do='read')
         idList = []
@@ -145,40 +148,57 @@ def whitelist(do):
             name, id, fold = item
             idList.append(id)
         lines = tools.read_from_file(xbmcvfs.File(source)).split('\n')
-        with open(vars.WHITELIST, 'a') as f:
+        with open(CONFIG.WHITELIST, 'a') as f:
             for item in lines:
                 try:
                     name, id, folder = eval(item)
                 except Exception as e:
-                    logging.log("Error Adding: '%s' / %s" % (item, str(e)), xbmc.LOGERROR)
+                    logging.log("Error Adding: '{0}' / {1}".format(item, str(e)), level=xbmc.LOGERROR)
                     continue
-                logging.log("%s / %s / %s" % (name, id, folder), xbmc.LOGDEBUG)
+                logging.log("{0} / {1} / {2}".format(name, id, folder))
                 if id not in idList:
                     count += 1
-                    writing = "['%s', '%s', '%s']" % (name, id, folder)
+                    writing = "['{0}', '{1}', '{2}']".format(name, id, folder)
                     if len(idList) + count > 1:
                         writing = "\n%s" % writing
                     f.write(writing)
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]%s Item(s) Added[/COLOR]" % (uservar.COLOR2, count))
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]{1} Item(s) Added[/COLOR]".format(CONFIG.COLOR2, count))
     elif do == 'export':
-        source = gui.DIALOG.browse(3, '[COLOR %s]Select where you wish to export the whitelist file[/COLOR]' % uservar.COLOR2, 'files', '.txt', False, False, vars.HOME)
-        logging.log(str(source), xbmc.LOGDEBUG)
+        source = gui.DIALOG.browse(3,
+                                   '[COLOR {0}]Select where you wish to export the whitelist file[/COLOR]'.format(CONFIG.COLOR2),
+                                   'files', '.txt', False, False, CONFIG.HOME)
+        logging.log(str(source))
         try:
-            xbmcvfs.copy(vars.WHITELIST, os.path.join(source, 'whitelist.txt'))
-            gui.DIALOG.ok(uservar.ADDONTITLE, "[COLOR %s]Whitelist has been exported to:[/COLOR]" % (uservar.COLOR2), "[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, os.path.join(source, 'whitelist.txt')))
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]Whitelist Exported[/COLOR]" % (uservar.COLOR2))
+            xbmcvfs.copy(CONFIG.WHITELIST, os.path.join(source, 'whitelist.txt'))
+            gui.DIALOG.ok(CONFIG.ADDONTITLE,
+                          "[COLOR {0}]Whitelist has been exported to:[/COLOR]".format(CONFIG.COLOR2),
+                          "[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, os.path.join(source, 'whitelist.txt')))
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]Whitelist Exported[/COLOR]".format(CONFIG.COLOR2))
         except Exception as e:
-            logging.log("Export Error: %s" % str(e), xbmc.LOGERROR)
-            if not gui.DIALOG.yesno(uservar.ADDONTITLE, "[COLOR %s]The location you selected isnt writable would you like to select another one?[/COLOR]" % uservar.COLOR2, yeslabel="[B][COLOR springgreen]Change Location[/COLOR][/B]", nolabel="[B][COLOR red]No Cancel[/COLOR][/B]"):
-                logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]Whitelist Export Cancelled[/COLOR]" % (uservar.COLOR2, e))
+            logging.log("Export Error: {0}".format(str(e)), level=xbmc.LOGERROR)
+            if not gui.DIALOG.yesno(CONFIG.ADDONTITLE,
+                                    "[COLOR {0}]The location you selected isn\'t writable would you like to select another one?[/COLOR]".format(CONFIG.COLOR2),
+                                    yeslabel="[B][COLOR springgreen]Change Location[/COLOR][/B]",
+                                    nolabel="[B][COLOR red]No Cancel[/COLOR][/B]"):
+                logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                                   "[COLOR {0}]Whitelist Export Cancelled[/COLOR]".format(CONFIG.COLOR2, e))
             else:
                 whitelist(do='export')
     elif do == 'clear':
-        if not gui.DIALOG.yesno(uservar.ADDONTITLE, "[COLOR %s]Are you sure you want to clear your whitelist?" % uservar.COLOR2, "This process can't be undone.[/COLOR]", yeslabel="[B][COLOR springgreen]Yes Remove[/COLOR][/B]", nolabel="[B][COLOR red]No Cancel[/COLOR][/B]"):
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]Clear Whitelist Cancelled[/COLOR]" % (uservar.COLOR2))
+        if not gui.DIALOG.yesno(CONFIG.ADDONTITLE,
+                                "[COLOR {0}]Are you sure you want to clear your whitelist?".format(CONFIG.COLOR2),
+                                "This process can't be undone.[/COLOR]",
+                                yeslabel="[B][COLOR springgreen]Yes Remove[/COLOR][/B]",
+                                nolabel="[B][COLOR red]No Cancel[/COLOR][/B]"):
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]Clear Whitelist Cancelled[/COLOR]".format(CONFIG.COLOR2))
             return
         try:
-            os.remove(vars.WHITELIST)
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]Whitelist Cleared[/COLOR]" % (uservar.COLOR2))
+            os.remove(CONFIG.WHITELIST)
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]Whitelist Cleared[/COLOR]".format(CONFIG.COLOR2))
         except:
-            logging.log_notify("[COLOR %s]%s[/COLOR]" % (uservar.COLOR1, uservar.ADDONTITLE), "[COLOR %s]Error Clearing Whitelist![/COLOR]" % (uservar.COLOR2))
+            logging.log_notify("[COLOR {0}]{1}[/COLOR]".format(CONFIG.COLOR1, CONFIG.ADDONTITLE),
+                               "[COLOR {0}]Error Clearing Whitelist![/COLOR]".format(CONFIG.COLOR2))
