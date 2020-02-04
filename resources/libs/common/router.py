7e370db8e29bafdfd55955ba6a5b17c6ecd7439d
drinfernoo <drinfernoo@yahoo.com>
2020-02-03 16:25:48 -0800

    :children_crossing: - Remove "Normal Install" option
    Close #54

--- resources/libs/common/router.py
+++ resources/libs/common/router.py
@@ -75,8 +75,8 @@ class Router:
         elif mode == 'install':  # Builds -> Fresh Install/Standard Install/Apply guifix
             from resources.libs.wizard import Wizard
 
-            if action in ['fresh', 'normal']:
-                Wizard().build(action, name)
+            if action == 'build':
+                Wizard().build(name)
             elif action == 'gui':
                 Wizard().gui(name)
             elif action == 'theme':  # Builds -> "Your Build" -> "Your Theme"
