ba838e35e2e28e253e3a51b798dd2bb4b643f35c
nikisb <nykyvi@gmail.com>
2020-08-29 14:19:14 +0200

    druga izmena

--- uservar.py
+++ uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR purple][B]Varadero[/B][/COLOR]Wizard'
 BUILDERNAME = 'OpenWizard'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it.
-BUILDFILE = 'http://'
+BUILDFILE = 'https://raw.githubusercontent.com/nikisb/plugin.program.openwizard/test/builds/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
